7dde5b9b3f3a1d9b92de7fd88a7942e029d0a245
salsqbla_ <nurulsalsabila315@gmail.com>
2026-07-16 12:28:08 +0700

    change icon

--- lib/constants/api.dart
+++ lib/constants/api.dart
@@ -1,20 +1,3 @@
-// class Api {
-//   // Emulator Android
-//   // static const String baseUrl = "http://10.0.2.2:3000/api/mobile";
-
-//  // ganti jadi IP laptop
-//   static const String baseUrl = "http://192.168.1.61:3000/api/mobile";
-
-//   static const login = "/auth/login";
-
-//   static const profile = "/profile";
-
-//   static const health = "/health";
-
-//   static const syncHealth = "/iot/status";
-//   static const history = "/api/iot/status";
-// }
-
 class Api {
   // ganti jdi ip laptop
   static const String baseUrl = "http://192.168.1.61:3000";
